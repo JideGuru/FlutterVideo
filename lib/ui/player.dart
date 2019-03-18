c05695f306cb78a021fc77de8e21b544ba5da8f9
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-18 19:44:45 +0100

    Added a video player class implemented with CHEWIE

--- lib/ui/player.dart
+++ lib/ui/player.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+// ignore: implementation_imports
+import 'package:chewie/src/chewie_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:video_player/video_player.dart';
 
 class Player extends StatefulWidget {
 
@@ -12,8 +19,50 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  TargetPlatform _platform;
+  VideoPlayerController _videoPlayerController;
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    File videoFile = new File(widget.video);
+    _videoPlayerController = VideoPlayerController.file(videoFile);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      aspectRatio: 3 / 2,
+      autoPlay: true,
+      looping: true,
+      // Try playing around with some of these other options:
+
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      autoInitialize: true,
+      fullScreenByDefault: true,
+    );
+
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Chewie(
+      controller: _chewieController,
+    );
   }
 }
